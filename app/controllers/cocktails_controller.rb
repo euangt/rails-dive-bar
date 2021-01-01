6e16462c72859dc1b6746561979b81c9f678ad55
Euan Gillespie-Taylor <euan.gillespie@outlook.com>
2021-01-01 15:39:54 +0000

    remove cocktail added

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -18,7 +18,13 @@ class CocktailsController < ApplicationController
     else
       render :new
     end
-end
+  end 
+
+  def destroy
+    @cocktail = Cocktail.find(params[:id])
+    @cocktail.destroy
+    redirect_to root_path
+  end
 
   private
 
